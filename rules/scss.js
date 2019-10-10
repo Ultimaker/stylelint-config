93d7bdf6eedb315acb84570e1380cd208b19d215
Matijs Brinkhuis <matijs@gmail.com>
2019-10-10 11:11:52 +0200

    Clean up SCSS rules
    ./scss.js should only extend
    ./rules/scss.js now contain the actual SCSS rules
    
    @extend and @debug are considered valid at-rules in SCSS but are now
    on the blacklist so they only generate one violation when used instead
    of two.

--- rules/scss.js
+++ rules/scss.js
@@ -1,5 +1,6 @@
 module.exports = {
     rules: {
+        'at-rule-blacklist': ['debug', 'extend'],
         'at-rule-no-unknown': [
             true,
             {
--- scss.js
+++ scss.js
@@ -1,30 +1,3 @@
 module.exports = {
     extends: ['./rules/scss.js', './index.js'],
-    rules: {
-        // At-rules
-        'at-rule-blacklist': ['debug', 'extend'],
-        'at-rule-no-unknown': [
-            true,
-            {
-                // this set excludes '@extend'
-                ignoreAtRules: [
-                    'use',
-                    'forward',
-                    'import',
-                    'mixin',
-                    'include',
-                    'function',
-                    'return',
-                    'error',
-                    'warn',
-                    'debug',
-                    'if',
-                    'else',
-                    'each',
-                    'for',
-                    'while',
-                ],
-            },
-        ],
-    },
 };
