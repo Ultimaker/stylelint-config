56d7086f3d7dcc8844cbe06fd1ce5afb65aa5697
matijs <matijs@gmail.com>
2020-02-12 17:33:39 +0100

    Remove @ultimaker/eslint-config
    This is just plain simple JS, the eslint:recommended rules + the
    defaults ESLint suggest upon init are plenty.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,21 @@
 module.exports = {
-    root: true,
-    extends: '@ultimaker/eslint-config',
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+    },
+    extends: 'eslint:recommended',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+    },
+    rules: {
+        'indent': ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+    },
 };
